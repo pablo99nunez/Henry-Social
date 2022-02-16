95e8639350e80090b569d2a990db3c569b3c8777
pablo99nunez <pablo99nunez@gmail.com>
2022-02-16 12:54:06 -0300

    User model

--- models/User.ts
+++ models/User.ts
@@ -1,4 +1,4 @@
-/* import {Schema, model} from 'mongoose';
+import {Schema, model} from 'mongoose';
 
 interface User{
     name: string,
@@ -6,9 +6,8 @@ interface User{
     username: string,
     password: string,
     avatar?: Buffer,    
-
+    createdAt: string
 }
-
 
 const userSchema = new Schema<User>({
     name:{
@@ -24,22 +23,6 @@ const userSchema = new Schema<User>({
         require:true
     },
     avatar: Buffer,
-    createdAt: new Date()
+    createdAt: new Date().toDateString()
 })
-export default new model<User>('User', userSchema)
- */
-import { Schema, model, connect } from 'mongoose';
-
-// 1. Create an interface representing a document in MongoDB.
-interface User {
-  name: string;
-  email: string;
-  avatar?: string;
-}
-
-// 2. Create a Schema corresponding to the document interface.
-const schema = new Schema<User>({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  avatar: String
-});
+export default model<User>('User', userSchema) 
