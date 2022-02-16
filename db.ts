597e786e8760c08658af2d7c32efc90873585b54
pablo99nunez <pablo99nunez@gmail.com>
2022-02-16 11:33:28 -0300

    feat: conexion a MongoAtlas

---
+++ db.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config.js'
+const mongoose = require('mongoose')
+
+function main() {
+  console.log(process.env.MONGO_PASSWORD)
+  mongoose
+    .connect(
+      `mongodb+srv://HenrySocial:${process.env.MONGO_PASSWORD}@cluster0.cvmsj.mongodb.net/HS-DB?retryWrites=true&w=majority`
+    ,{
+      useNewUrlParser:true,
+      useUnifiedTopology:true
+    })
+    .then(() => {
+      console.log('MongoDB connected succesfully');
+    })
+    .catch((e:any) => {
+      console.log(e);
+    });
+}
+main();
