b7b1e26eebf07a10c7af684209100403454729e0
DavidDeadly <jdrueda513@gmail.com>
2022-02-17 16:34:53 -0500

    refactor: sorting things up after the merge

--- client/src/Pages/Login/Login.tsx
+++ client/src/Pages/Login/Login.tsx
@@ -1,111 +1,108 @@
-
 import React, {  useEffect, useState } from 'react'
 import { Navigate, useNavigate } from 'react-router-dom'
-import {auth} from '../../../../src/services/firebase/firebase'
-import {signUpWithEmail,signUpWithGmail,signUpWithGitHub} from '../../../../src/services/firebase/login-methods'
-import {IUser} from '../../../../src/models/User'
 
-export default function Login() {
+import { auth } from '../../../../src/services/firebase/firebase'
+import { signUpWithEmail, signUpWithGmail, signUpWithGitHub } from '../../../../src/services/firebase/login-methods'
+import { IUser } from '../../../../src/models/User'
+import style from './Login.module.scss';
 
-	const [input,setInput] = useState<IUser> ({
-    name:"",
-    username:"",
-    password:"",
-    email:"",
-    avatar:"",
+export default function Login(): JSX.Element {
+
+	const [ input,setInput ] = useState<IUser> ({
+    name: "",
+    username: "",
+    password: "",
+    email: "",
+    avatar: "",
     createdAt: {}
   })
-  const [loading, setLoading] = useState(false)
-  const navigate=useNavigate()
+  const [ loading, setLoading ] = useState(false)
+  const navigate = useNavigate()
 
-  useEffect(()=>{
-    if(!!auth.currentUser){
-      navigate("/home")
-      
-    }else console.log()
-  },[])
+  useEffect(() => {
+    if(!!auth.currentUser) navigate("/home")
+  }, [])
 
-  const handleSubmit = async (e:React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault()
 
     if(!!auth.currentUser){
       alert("Ya estas logeado, redirigiendo a Home")
       navigate("/home")
-    }else{
-
-      try{
+    } else {
+      try {
         setLoading(true)
-        
         await signUpWithEmail(input)
         alert("Usuario creado con exito")
         navigate("/home")
         setLoading(false)
-      }catch(e){
+      } catch(e) {
         alert(e)
         setLoading(false)
       }
     }
 	}
-  const handleLogin = async (cb:Function)=>{
+
+  const handleLogin = async (cb: Function)=>{
     if(!!auth.currentUser){
       alert("Ya estas logeado, redirigiendo a Home")
       navigate("/home")
-    }else{
-
-      try{
+    } else {
+      try {
         const result= await cb()
         if(!!result){
           alert("Usuario logueado con exito")
           navigate("/home")
         }
-      }catch(e){
+      } catch(e) {
         alert(e)
       }
     }
   }
-  function handleInputChange(e:React.ChangeEvent<HTMLInputElement>){
-    const property=e.target.name
+
+  function handleInputChange(e: React.ChangeEvent<HTMLInputElement>){
+    const property = e.target.name
     console.log(e)
     if(property===null) throw new Error() 
-    else{
-
-      setInput({...input,
-                 [property]:e.target.value}
-                )
-        
-      }
-    
+    else {
+      setInput({
+        ...input,
+        [property]:e.target.value
+      })
+    }
   }
 
   return (
     <>
-
-<div id="cont">
+    <div id={style.cont}>
       <header>
-        <div id="title-cont">
+        <div id={style.title_cont}>
           <img src="#" alt="icon" />
-          <h1>| Social</h1>
+          <h1> | Social </h1>
         </div>
-        <div id="btns-cont">
-          <button id="sign-in">Registrarse</button>
-          <button id="log-in">Iniciar sesion</button>
+        <div id={style.btns_cont}>
+          <button id={style.sign_in}> Registrarse </button>
+          <button id={style.log_in}> Iniciar sesión </button>
         </div>
       </header>
-      <div id="form-cont">
+      <div id={style.form_cont}>
         <form onSubmit={handleSubmit}>
-          <h3>Hola, ¡Bienvenida/o a Henry Social!</h3>
-          <input type="email" id="email" name="email" placeholder="Ingresa tu email" onChange={handleInputChange}/>
-          <input type="password" id="pass" name="password" placeholder="Ingresa tu contraseña" onChange={handleInputChange}/>
+          <h3> Hola, ¡Bienvenida/o a Henry Social! </h3>
+          <input type="email" id={style.email} name="email" placeholder="Ingresa tu email" onChange={handleInputChange}/>
+          <input type="password" id={style.pass} name="password" placeholder="Ingresa tu contraseña" onChange={handleInputChange}/>
           <button disabled={loading} type="submit"> Sign Up </button>
         </form>
-        <button id="cls-btn">X</button>
-        <div id="alt-cont">
-          <button id="google-btn">Google</button>
-          <button id="github-btn">Github</button>
+        <div id={style.alt_cont}>
+          <button id={style.google_btn}
+            onClick={() => handleLogin(signUpWithGmail)}
+          > With Google </button>
+          <button id={style.github_btn}
+            onClick={() => handleLogin(signUpWithGitHub)}
+          > With Github </button>
         </div>
       </div>
     </div>
-    
+
     {/* <form onSubmit={handleSubmit}>
         <input type="email" name="email"  onChange={handleInputChange}/>
         <input type="password" name="password"  onChange={handleInputChange}/>
