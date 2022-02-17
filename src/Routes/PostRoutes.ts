e29f8f8e8021a4fa8b195c13fdc0cfaf4573ce56
pablo99nunez <pablo99nunez@gmail.com>
2022-02-17 18:24:45 -0300

    feat: add route that create a post

--- src/Routes/PostRoutes.ts
+++ src/Routes/PostRoutes.ts
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import { Model } from 'mongoose';
 import Post from '../models/Post';
 
 const router = Router();
+// let Post:Model<''>
 
 router.get('/posts', async (req, res) => {
   const posts = await Post.find({});
   res.json(posts);
+});
+
+router.post('/post', async (req, res) => {
+  let post = await Post.create(req.body);
+  res.json(post);
 });
