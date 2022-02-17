00eadc49c7dbca64adc4ee386d0290ee54722616
pablo99nunez <pablo99nunez@gmail.com>
2022-02-17 18:30:16 -0300

    feat: add delete post route

--- src/Routes/PostRoutes.ts
+++ src/Routes/PostRoutes.ts
@@ -1,16 +1,33 @@
 import { Router } from 'express';
 import { Model } from 'mongoose';
-import Post from '../models/Post';
+// import Post from '../models/Post';
 
 const router = Router();
-// let Post:Model<''>
+let Post: Model<''>;
 
 router.get('/posts', async (req, res) => {
-  const posts = await Post.find({});
-  res.json(posts);
+  try {
+    const posts = await Post.find({});
+    res.json(posts);
+  } catch (e) {
+    res.status(401).json({ error: e });
+  }
 });
 
 router.post('/post', async (req, res) => {
-  let post = await Post.create(req.body);
-  res.json(post);
+  try {
+    let post = await Post.create(req.body);
+    res.json(post);
+  } catch (e) {
+    res.status(401).json({ error: e });
+  }
+});
+router.delete('/post', async (req, res) => {
+  try {
+    const { _id } = req.body;
+    let post = await Post.deleteOne({ _id });
+    res.json;
+  } catch (e) {
+    res.status(401).json({ error: e });
+  }
 });
