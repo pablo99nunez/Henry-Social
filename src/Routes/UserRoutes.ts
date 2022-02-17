b9bb671985ac0641303848d22ddbd44754bf17de
pablo99nunez <pablo99nunez@gmail.com>
2022-02-17 18:51:07 -0300

    test

--- src/Routes/UserRoutes.ts
+++ src/Routes/UserRoutes.ts
@@ -1,24 +1,33 @@
-import { Router } from "express";
-import User from "../models/User";
+import { Router } from 'express';
+import User from '../models/User';
 
-const router = Router()
+const router = Router();
 
-
-router.post("/user",(req,res)=>{
-    const {name,username,email,avatar,createAt} = req.body
-    User.create({name,username,email,avatar,createAt}).then(result=>{
-        res.status(201).json(result)
-    }).catch(e=>res.status(400).json({error:e}))
-})
-
-router.get("/user",(req,res)=>{
-    const {query} = req.body
-    User.findOne(query).then(e=>{
-        res.json(e)
-    }).catch(e=>{
-        res.status(400).send(e)
+router.post('/user', (req, res) => {
+  const { name, username, email, avatar, createAt } = req.body;
+  User.create({ name, username, email, avatar, createAt })
+    .then((result) => {
+      res.status(201).json(result);
     })
-   
-})
+    .catch((e) => res.status(400).json({ error: e }));
+});
+router.get('/users', async (req, res) => {
+  try {
+    let users = await User.find({});
+    res.json(users);
+  } catch (e) {
+    res.status(401).json({ error: e });
+  }
+});
+router.post('/findUser', (req, res) => {
+  const { query } = req.body;
+  User.findOne(query)
+    .then((e) => {
+      res.json(e);
+    })
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+});
 
-export default router
+export default router;
