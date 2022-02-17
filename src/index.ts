bd8f131aa176e0d5197b8c64855be25c56cc1db6
pablo99nunez <pablo99nunez@gmail.com>
2022-02-17 20:13:21 -0300

    chore: add ts-node to deps

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 import express from 'express';
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
 import 'dotenv/config.js';
 import router from './Routes/UserRoutes';
 
 const app = express();
-
 app.use(cors());
 
-app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-app.use(bodyParser.json({ limit: '50mb' }));
+// app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+// app.use(bodyParser.json({ limit: '50mb' }));
+app.use(express.json());
 app.use(morgan('dev'));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
@@ -29,6 +29,4 @@ app.listen(process.env.PORT, () => {
   console.log('Server listening at ' + process.env.PORT);
 });
 
-app.set('views', __dirname + '/public');
-app.set('view engine', 'pug');
 export default app;
