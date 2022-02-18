706a8db90f4dadb9c7cc7bd3805a72e696d0c7db
pablo99nunez <pablo99nunez@gmail.com>
2022-02-18 04:30:20 -0300

    fix: key error

--- client/src/components/followBar/FollowBar.tsx
+++ client/src/components/followBar/FollowBar.tsx
@@ -1,39 +1,35 @@
 import react from 'react';
 import { Follows } from './followExample';
-import './followBar.scss'
-
+import './followBar.scss';
 
 export default function FollowBar() {
-    let nuevo: Follows[] = [
-        {name: 'Alguien', avatar:'https://avatars.githubusercontent.com/u/9113740?v=4'}, 
-        {name:'Alejandro', avatar:'https://avatars.githubusercontent.com/u/78025342?v=4'},
-        {name: 'Rei', avatar:'https://avatars.githubusercontent.com/u/68031974?v=4'}
-    ]
-    interface Estilos {
-        background: string
-    }
+  let nuevo: Follows[] = [
+    { name: 'Alguien', avatar: 'https://avatars.githubusercontent.com/u/9113740?v=4' },
+    { name: 'Alejandro', avatar: 'https://avatars.githubusercontent.com/u/78025342?v=4' },
+    { name: 'Rei', avatar: 'https://avatars.githubusercontent.com/u/68031974?v=4' },
+  ];
+  interface Estilos {
+    background: string;
+  }
 
-    function estilado(entrada:any) {
-        let estilos: Estilos ={
-            background: `url(${entrada}) center/contain no-repeat`,
+  function estilado(entrada: any) {
+    let estilos: Estilos = {
+      background: `url(${entrada}) center/contain no-repeat`,
+    };
+    return estilos;
+  }
 
-        }
-        return estilos
-    }
+  let prueba;
 
-    let prueba;
-    
-    return(
-        <div>
-        {
-            nuevo && nuevo.map(x => 
-                <div className='user'>
-                <p>{x.name}</p>
-                <div className='image' style={prueba=estilado(x.avatar)}>
-                </div>
-                </div>
-                )
-        }
-        </div>
-    )
+  return (
+    <div>
+      {nuevo &&
+        nuevo.map((x) => (
+          <div className="user" key={x.name}>
+            <p>{x.name}</p>
+            <div className="image" style={(prueba = estilado(x.avatar))}></div>
+          </div>
+        ))}
+    </div>
+  );
 }
