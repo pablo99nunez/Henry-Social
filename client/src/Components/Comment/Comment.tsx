b96f791a48a0ede59177b77ff17e6e0c842ff72d
GianmarcoVc <gianmarcovalentinc@gmail.com>
2022-02-18 23:38:06 -0500

    feat: Add path get post/:id

--- client/src/Components/Comment/Comment.tsx
+++ client/src/Components/Comment/Comment.tsx
@@ -1,50 +1,43 @@
 import './Comment.scss'
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { FaRegHeart, FaRegComments, FaEllipsisH, FaBan, FaRegFrown, FaHeart } from 'react-icons/fa'
+import { FaRegComment, FaEllipsisH, FaBan, FaRegFrown } from 'react-icons/fa'
+import Like from '../Like/Like'
 
 const Comment = ({key, data} : any) => {
 
-  const [ comment, setComment ] = useState(
-    {
-      liked: false,
-      options: false
-    }
-  )
+  const [ options, setOptions ] = useState(false)
 
   return (
     <div className="comment" key={key}>
 
       <div className="picture">
-        <img className='photo' src={data.img} alt={data.name}/>
+        <img className='photo' src={data.img} alt={data.author}/>
       </div>
 
       <div className="info">
 
         <div className="author">
-          <Link className='name' to={`/profile/${data.id}`}>{data.name}</Link>
-          <span className='cohorteDate'>{data.cohorte} · {data.date}</span>
+          <Link className='name' to={`/profile/${data.postId}`}>{data.author}</Link>
+          <span className='cohorteDate'>{data.cohorte} · {data.postTime}</span>
         </div>
 
         <div className="content">
-          <p className='desc'>{data.content}</p>
+          <p className='desc'>{data.text}</p>
         </div>
 
         <div className="likesComments">
-          <div className="sizeLikes" onClick={() => setComment({...comment, liked: !comment.liked})}>
-            {comment.liked 
-              ? <FaHeart title='Likes' className='fas icon'/>
-              : <FaRegHeart title='Likes' className='icon'/>
-            }
-            {data.countLikes &&
+          <div className="sizeLikes">
+            <Like/>
+            {/* {data.countLikes &&
               <p className='count'>{data.countLikes}</p>
-            }
+            } */}
           </div>
           {/* Al presionar en el icono o el numero de comentarios llevaria a tipo un posteo
               con el comentario y sus comentarios */}
-          {/* <Link to={${data.name}/posts${data.id}} className="sizeComments"> */}
-          <div className="sizeComments">
-            <FaRegComments title='Comentarios' className={`far icon`}/>
+          {/* <Link to={${data.author}/posts${data.postId}} className="sizeComments"> */}
+          <div className="sizeComments" title='Comentarios'>
+            <FaRegComment/>
             {data.countComments &&
               <p className='count'>{data.countComments}</p>
             }
@@ -54,21 +47,20 @@ const Comment = ({key, data} : any) => {
       </div>
           
       <div className="options">
-        <FaEllipsisH title='Más' className='fas fa-ellipsis-h icon' onClick={() => setComment({...comment, options: true})}/>
+        <FaEllipsisH title='Más' className='icon' onClick={() => setOptions(true)}/>
         <div 
-          className={`menu ${comment.options ? 'view' : 'hide'}`}
+          className={`menu ${options ? 'view' : 'hide'}`}
           onMouseOver= {() => document.onclick = null}
           onMouseOut={() => document.onclick = () => {
-            setComment({...comment, options:false})
+            setOptions(false)
             return document.onclick = null
           }} 
         >
           <p className='item'>
-            <FaRegFrown className='fas fa-frown'/>
-            Este comentario no es útil.
+            <FaRegFrown/>Este comentario no es útil.
           </p>
           <p className='item'>
-            <FaBan className='fas fa-ban'/>Bloquear usuario
+            <FaBan/>Bloquear usuario
           </p>
         </div>
       </div>
