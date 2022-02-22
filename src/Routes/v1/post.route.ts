0b2c3e37841ba627d0ae8e04fbd91d9c7f468fc5
pablo99nunez <pablo99nunez@gmail.com>
2022-02-22 18:09:26 -0300

    fix: ubication follow bar

---
+++ src/Routes/v1/post.route.ts
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+router.post("/", (req: any, res: any) => {
+  return res.json("Post agregado");
+});
+
+router.delete("/", (req: any, res: any) => {});
