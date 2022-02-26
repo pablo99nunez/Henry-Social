3211f984479e1d3d1673a660d18c6a4368ae9c90
pablo99nunez <pablo99nunez@gmail.com>
2022-02-26 14:54:02 -0300

    feat: Now can upload Files on type Posts

--- client/src/Components/ModalAddPost/AddPost.tsx
+++ client/src/Components/ModalAddPost/AddPost.tsx
@@ -4,8 +4,10 @@ import { useDispatch } from "react-redux";
 import useUser from "../../Hooks/useUser";
 import { getPosts } from "../../redux/actions/actions";
 import { InfoAlert } from "../Alert/Alert";
-import { FaUpload, FaCheck } from 'react-icons/fa'
+import { FaUpload, FaCheck } from "react-icons/fa";
 import styles from "./AddPost.module.scss";
+import { uploadFile } from "../../../../src/services/firebase/Helpers/uploadFile";
+import { IPost } from "../../../../src/models/Post";
 
 type Props = {
   // eslint-disable-next-line @typescript-eslint/ban-types
@@ -16,32 +18,42 @@ const AddPost: FC<Props> = ({ setOpen }) => {
   const user = useUser();
   const dispatch = useDispatch();
 
-  const [typePost, setTypePost] = useState('normal')
-  const [post, setPost] = useState({
-    text: '',
-    image: '',
-    company: '',
-    position: '',
-    companyLink: '',
-    companyImage: '',
-    salary: '',
-    costoClases: '',
-    temasClases: '',
-    tecnologíaClases: '',
-  })
+  const [typePost, setTypePost] = useState("normal");
+  const [post, setPost] = useState<IPost>({
+    text: "",
+    image: "",
+    company: "",
+    position: "",
+    companyLink: "",
+    companyImage: "",
+    salary: 0,
+    costoClases: "",
+    temasClases: "",
+    tecnologíaClases: "",
+  });
 
-  const handleChange = (e: React.FormEvent<HTMLFormElement>) => {
-    setPost({...post, [e.target.name]: e.target.value});
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.name === "companyImage" && e.target.files)
+      setPost({ ...post, [e.target.name]: e.target.files[0] });
+    else setPost({ ...post, [e.target.name]: e.target.value });
   };
 
   const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    let name = e.target.name
-    if(name === typePost) {return setTypePost('normal')}
-    setTypePost(name)
+    const name = e.target.name;
+    if (name === typePost) {
+      return setTypePost("normal");
+    }
+    setTypePost(name);
   };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const downloadURL =
+      post.companyImage instanceof File
+        ? await uploadFile(post.companyImage)
+        : post.companyImage;
+
     if (user)
       axios
         .post(`/post`, {
@@ -49,7 +61,7 @@ const AddPost: FC<Props> = ({ setOpen }) => {
           company: post.company,
           position: post.position,
           companyLink: post.companyLink,
-          companyImage: post.companyImage,
+          companyImage: downloadURL,
           salary: post.salary,
           author: user,
           typePost,
@@ -68,145 +80,157 @@ const AddPost: FC<Props> = ({ setOpen }) => {
 
   const types = [
     {
-      abr: 'multimedia',
-      text: 'Foto / Video'
+      abr: "multimedia",
+      text: "Foto / Video",
     },
     {
-      abr: 'boom',
-      text: 'Boom'
+      abr: "boom",
+      text: "Boom",
     },
     {
-      abr: 'empleo',
-      text: 'Ofertas de empleo'
+      abr: "empleo",
+      text: "Ofertas de empleo",
     },
     {
-      abr: 'servicio',
-      text: 'Servicio'
-    }
+      abr: "servicio",
+      text: "Servicio",
+    },
     // {
     //   abr: 'pregunta',
     //   text: 'Pregunta'
     // },
-  ]
+  ];
 
   return (
     <form
-      onSubmit={e => handleSubmit(e)}
-      onChange={e => handleChange(e)}
+      onSubmit={(e) => handleSubmit(e)}
+      onChange={(e) => handleChange(e)}
       className={styles.modal_add_post}
-     >
+    >
       <div className={styles.add_post_content}>
-        {typePost !== 'normal' && typePost !== 'multimedia' &&
+        {typePost !== "normal" && typePost !== "multimedia" && (
           <div className={styles.content__inputs}>
-            {typePost === 'servicio' ?
-            <>
-              <input 
-                type="text" 
-                name='tecnologíaClases'
-                placeholder='Tecnología'
-                defaultValue={post.tecnologíaClases}
-              />  
-              <input 
-                type="text" 
-                name='temasClases'
-                placeholder='Temas'
-                defaultValue={post.temasClases}
-              />  
-              <input 
-                name='costoClases'
-                type="number" 
-                defaultValue={post.costoClases}
-                placeholder='Costo de las clases'
-              />  
-            </> :
-            <>
-              <input 
-                type="text" 
-                name="company" 
-                defaultValue={post.company}
-                placeholder="Nombre de la Empresa"
-                required
-              />
-              <input 
-                type="text" 
-                name="position" 
-                defaultValue={post.position}
-                placeholder="Rol en la Empresa"
-                required
-              />
-              <input 
-                type='url'
-                name='companyImage'
-                defaultValue={post.companyImage}
-                placeholder='Imagen de la empresa'
-              />
-            </>
-            }
-            {typePost === 'empleo' &&
-            <>
-              <input 
-                name="companyLink" 
-                type="url" 
-                defaultValue={post.companyLink}
-                placeholder="Link del Empleo"
-                required
-              />
-              <input 
-                min='0'
-                type="number" 
-                name="salary" 
-                defaultValue={post.salary}
-                placeholder="Salario (Opcional)"
-              />
-            </>
-            }
+            {typePost === "servicio" ? (
+              <>
+                <input
+                  type="text"
+                  name="tecnologíaClases"
+                  placeholder="Tecnología"
+                  defaultValue={post.tecnologíaClases}
+                />
+                <input
+                  type="text"
+                  name="temasClases"
+                  placeholder="Temas"
+                  defaultValue={post.temasClases}
+                />
+                <input
+                  name="costoClases"
+                  type="number"
+                  defaultValue={post.costoClases}
+                  placeholder="Costo de las clases"
+                />
+              </>
+            ) : (
+              <>
+                <input
+                  type="text"
+                  name="company"
+                  defaultValue={post.company}
+                  placeholder="Nombre de la Empresa"
+                  required
+                />
+                <input
+                  type="text"
+                  name="position"
+                  defaultValue={post.position}
+                  placeholder="Rol en la Empresa"
+                  required
+                />
+
+                <input
+                  type="file"
+                  accept=".png"
+                  name="companyImage"
+                  defaultValue={post.companyImage}
+                  placeholder="Imagen de la empresa"
+                />
+              </>
+            )}
+            {typePost === "empleo" && (
+              <>
+                <input
+                  name="companyLink"
+                  type="url"
+                  defaultValue={post.companyLink}
+                  placeholder="Link del Empleo"
+                  required
+                />
+                <input
+                  min="0"
+                  type="number"
+                  name="salary"
+                  defaultValue={post.salary}
+                  placeholder="Salario (Opcional)"
+                />
+              </>
+            )}
           </div>
-        }
+        )}
         <div className={styles.content__textImage}>
           <textarea
             name="text"
-            placeholder={typePost === 'boom' 
-            ? 'Cuentanos tu emoción en el comienzo de tu nueva aventura' 
-            : typePost === 'empleo' ? 'Explica más sobre este empleo' 
-            : typePost === 'servicio' ? 'Cuentanós sobre tus clases y sobre quien eres' 
-            : '¿Que estas pensando?'}
-            className={post.text ? styles.active : ''}
+            placeholder={
+              typePost === "boom"
+                ? "Cuentanos tu emoción en el comienzo de tu nueva aventura"
+                : typePost === "empleo"
+                ? "Explica más sobre este empleo"
+                : typePost === "servicio"
+                ? "Cuentanós sobre tus clases y sobre quien eres"
+                : "¿Que estas pensando?"
+            }
+            className={post.text ? styles.active : ""}
           ></textarea>
-          {typePost === 'multimedia' &&
+          {typePost === "multimedia" && (
             <div className={styles.boxImage}>
-              <label 
+              <label
                 htmlFor="image"
-                title={post.image ? 'Cambia tu archivo' : 'Selecciona tu archivo'}
+                title={
+                  post.image ? "Cambia tu archivo" : "Selecciona tu archivo"
+                }
               >
-                {post.image ?
+                {post.image ? (
                   <>
-                    <FaCheck/>
+                    <FaCheck />
                     <p>{post.image.slice(12)}</p>
                   </>
-                : <FaUpload/>}
+                ) : (
+                  <FaUpload />
+                )}
               </label>
               <input type="file" name="image" id="image" />
             </div>
-          }
+          )}
         </div>
       </div>
       <div className={styles.add_post_tags}>
-        {types.map((t,i) =>
-          <button 
+        {types.map((t, i) => (
+          <button
             key={i}
             name={t.abr}
             type="button"
-            onClick={e => handleClick(e)}
+            onClick={(e) => handleClick(e)}
             className={typePost === t.abr ? styles.select : styles.deselect}
-          >{t.text}</button>
-        )}
+          >
+            {t.text}
+          </button>
+        ))}
       </div>
       <div className={styles.add_post_buttons}>
-        <input type='submit' value='Publicar'/>
-        <button 
-          type="button" 
-          onClick={() => setOpen(false)}
-        >Cancelar</button>
+        <input type="submit" value="Publicar" />
+        <button type="button" onClick={() => setOpen(false)}>
+          Cancelar
+        </button>
       </div>
     </form>
   );
