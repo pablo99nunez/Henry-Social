b5b1de46ee6e2eef7dad237f59b3a47d5bd6ad09
pablo99nunez <pablo99nunez@gmail.com>
2022-02-26 14:53:42 -0300

    fix:Seguido

--- client/src/Components/Seguido/Seguido.tsx
+++ client/src/Components/Seguido/Seguido.tsx
@@ -1,13 +1,12 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { ObjectFlags } from "typescript";
 import Avatar from "../Avatar/Avatar";
 import Placeholder from "./Placeholder";
 import styles from "./Seguido.module.scss";
 
 const Seguido = ({ username, key }: any) => {
-  let [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     axios
@@ -16,7 +15,7 @@ const Seguido = ({ username, key }: any) => {
         return setUser(data.data);
       })
       .catch((error) => console.error("Error:", error));
-  }, []);
+  }, [username]);
 
   return user ? (
     <Link to={`/profile/${user?.username}`} className={styles.user} key={key}>
