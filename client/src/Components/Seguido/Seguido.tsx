7ab6c9902f34a4d9aef09cfeba58b128c8cab94d
pablo99nunez <pablo99nunez@gmail.com>
2022-02-26 14:06:48 -0300

    fix: avatar

--- client/src/Components/Seguido/Seguido.tsx
+++ client/src/Components/Seguido/Seguido.tsx
@@ -1,13 +1,13 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { ObjectFlags } from 'typescript';
-import Placeholder from './Placeholder';
-import styles from './Seguido.module.scss'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { ObjectFlags } from "typescript";
+import Avatar from "../Avatar/Avatar";
+import Placeholder from "./Placeholder";
+import styles from "./Seguido.module.scss";
 
-const Seguido = ({username, key} : any) => {
-
-  let [user, setUser] = useState(null) 
+const Seguido = ({ username, key }: any) => {
+  let [user, setUser] = useState(null);
 
   useEffect(() => {
     axios
@@ -16,14 +16,16 @@ const Seguido = ({username, key} : any) => {
         return setUser(data.data);
       })
       .catch((error) => console.error("Error:", error));
-  }, [])
-  
+  }, []);
+
   return user ? (
     <Link to={`/profile/${user?.username}`} className={styles.user} key={key}>
       <p>{user?.name}</p>
-      <img src={user?.avatar} alt="user avatar" />
+      <Avatar avatar={user?.avatar}></Avatar>
     </Link>
-  ) : <Placeholder/>
-}
+  ) : (
+    <Placeholder />
+  );
+};
 
 export default Seguido;
