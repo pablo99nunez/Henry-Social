5b8b9b1490917d8e46c411294aa1a8c21fa2fb59
pablo99nunez <pablo99nunez@gmail.com>
2022-03-01 13:56:25 -0300

    fix:Chat

--- client/src/Components/Chat/Chat.tsx
+++ client/src/Components/Chat/Chat.tsx
@@ -1,61 +1,80 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import useUser from "../../Hooks/useUser";
 import io from "socket.io-client";
 import style from "./Chat.module.scss";
 import { BiChevronsUp } from "react-icons/bi";
+import Avatar from "../Avatar/Avatar";
+import { motion } from "framer-motion";
 
-const socket= io("http://localhost:3001");
+const socket = io("http://localhost:3001");
 
 const Chat = () => {
   const user = useUser();
-  const [open,setOpen] = useState(false);
-  const [message,setMessage] = useState("");
-  const [listMessage,setListMessage] = useState<any[]>([])
-  const handleClick = (e:any) => {
-    setOpen(prevState => (!prevState))
-  }
+  const [open, setOpen] = useState(false);
+  const [message, setMessage] = useState("");
+  const scrollToMe = useRef<HTMLDivElement>(null);
+  const [listMessage, setListMessage] = useState<any[]>([]);
+  const handleClick = (e: any) => {
+    setOpen((prevState) => !prevState);
+  };
 
-  const SendMessage =  () => {
+  const SendMessage = () => {
     if (message) {
       const messageData = {
-          author:user?.username,
-          message: message,
-          time: new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes()
-      }
-      socket.emit("send_message",messageData)
-      setMessage("")
+        author: user?.username,
+        name: user?.name,
+        avatar: user?.avatar,
+        message: message,
+        time:
+          new Date(Date.now()).getHours() +
+          ":" +
+          new Date(Date.now()).getMinutes(),
+      };
+      socket.emit("send_message", messageData);
+      setMessage("");
     }
-  }
-  useEffect(()=>{
-      socket.on("receive_message", (data) => {
-        setListMessage((listMessage)=>[...listMessage,data])
-      })
-  },[socket,listMessage])
+  };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setListMessage([...listMessage, data]);
+    });
+    scrollToMe.current?.scrollIntoView({ block: "end", behavior: "smooth" });
+  }, [socket, listMessage]);
 
   return (
     <>
-      {!open ? 
-      ( 
-        <div onClick={e => handleClick(e)} className={style.chat}>
-          Live Chat{" "}
-          <span>
-          {" "}
-          <BiChevronsUp />
-          </span>
-        </div>
-      )
-      : (
-      <div  className={style.chat_window}>
-        <div onClick={e => handleClick(e)} className={style.chat_header}>
+      <motion.div
+        animate={
+          !open
+            ? {
+                y: 375,
+              }
+            : { y: 0 }
+        }
+        className={style.chat_window}
+      >
+        <div onClick={(e) => handleClick(e)} className={style.chat_header}>
           <p>Live Chat</p>
+          <motion.div animate={!open ? { rotateZ: 0 } : { rotateZ: 180 }}>
+            <BiChevronsUp></BiChevronsUp>
+          </motion.div>
         </div>
-        <div className={style.chat_body}>        
-          {listMessage.map((msg)=>(
-            <div 
+        <div className={style.chat_body}>
+          {listMessage.map((msg) => (
+            <div
               className={style.message}
-              id={user?.username === msg.author ? `${style.you}` :  `${style.other}`}
+              id={
+                user?.username === msg.author
+                  ? `${style.you}`
+                  : `${style.other}`
+              }
             >
-              <div>
+              <Avatar avatar={msg.avatar}></Avatar>
+              <div className={style.message_wrap}>
+                <p>
+                  <strong>{msg.name}</strong>
+                </p>
                 <div className={style.message_content}>
                   <p>{msg.message}</p>
                 </div>
@@ -65,14 +84,15 @@ const Chat = () => {
               </div>
             </div>
           ))}
+          <div ref={scrollToMe}></div>
         </div>
         <div className={style.chat_footer}>
           <input
             type="text"
             value={message}
             placeholder="Type Message..."
-            onChange={(e)=>{
-              setMessage(e.target.value)
+            onChange={(e) => {
+              setMessage(e.target.value);
             }}
             onKeyPress={(e) => {
               e.key === "Enter" && SendMessage();
@@ -80,9 +100,8 @@ const Chat = () => {
           />
           <button onClick={SendMessage}>&#9658;</button>
         </div>
-      </div>
+      </motion.div>
       )
-    }
     </>
   );
 };
