8c34311daeadc9414ca2477f8467f79b52a12712
DavidDeadly <jdrueda513@gmail.com>
2022-03-04 17:37:28 -0500

    fix: When waiting for email verification doesn't show the error page

--- client/src/Pages/NotFound/NotFound.tsx
+++ client/src/Pages/NotFound/NotFound.tsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom'
 import useUser from "../../Hooks/useUser";
 import styles from './NotFound.module.scss'
 import NavSearch from '../../Components/NavSearch/NavSearch' 
+import LoadingPage from '../../Components/LoadingPage/LoadingPage';
 
 const NotFound = () => {
 
@@ -9,17 +10,20 @@ const NotFound = () => {
   document.title = '404: Not Found | Henry Social'
 
   return (
-    <div className={styles.notFound}>
+    <>{
+      !user ? <LoadingPage/>
+      : <div className={styles.notFound}>
       <NavSearch />
-      <div className={styles.content}>
-        <h1>404</h1>
-        <h2>No esperebamos esta visita,<br/> estas opciones podrían ayudarte</h2>
-        <div className={styles.links}>
-          <Link to='/'>Ir a Inicio</Link>
-          {user && <Link to={`/profile/${user?.username}`}>Ver Perfil</Link>}
+        <div className={styles.content}>
+          <h1>404</h1>
+          <h2>No esperebamos esta visita,<br/> estas opciones podrían ayudarte</h2>
+          <div className={styles.links}>
+            <Link to='/'>Ir a Inicio</Link>
+            {user && <Link to={`/profile/${user?.username}`}>Ver Perfil</Link>}
+          </div>
         </div>
       </div>
-    </div>
+    }</>
   )
 }
 
