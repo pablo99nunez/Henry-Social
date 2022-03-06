d973a47e8c18471be0ed0a81032c001e86939449
pablo99nunez <pablo99nunez@gmail.com>
2022-03-06 15:51:26 -0300

    fix: Routes to firebase

--- client/src/Components/Post/Interactions/Interactions.tsx
+++ client/src/Components/Post/Interactions/Interactions.tsx
@@ -1,5 +1,5 @@
-import React, {useState} from "react";
-import { BsChatSquareDots,BsTrophy } from "react-icons/bs";
+import React, { useState } from "react";
+import { BsChatSquareDots, BsTrophy } from "react-icons/bs";
 
 import { IoArrowRedoOutline, IoArrowRedoSharp } from "react-icons/io5";
 import { IPost } from "../../../../../src/models/Post";
@@ -24,40 +24,40 @@ export default function Interactions({
   setOpenShare,
   shared,
 }: Props) {
-    return (
-        <div className={style.post_interacciones}>
-            <div className={style.post_like_comments}>
-                <Like post={post}></Like>
-                <div
-                    className={style.post_icon}
-                    onClick={() => setOpenComment(!openComment)}
-                >
-                    <div className={style.post_icon}>
-                        <BsChatSquareDots />
-                        <span>{post?.numComments}</span>
-                    </div>
-
-                </div>
-                <div 
-                className={style.post_icon}
-                onClick={() => {setOpenShare(!openShare); contenedor(post);}}
-                >
-                <IoArrowRedoOutline />
-                <span>{post?.nShares}</span>
-                </div>
-                <div className={style.post_icon}>
-                    <BsTrophy/>
-                </div>
-            </div>
+  return (
+    <div className={style.post_interacciones}>
+      <div className={style.post_like_comments}>
+        <Like post={post}></Like>
+        <div
+          className={style.post_icon}
+          onClick={() => setOpenComment(!openComment)}
+        >
+          <div className={style.post_icon}>
+            <BsChatSquareDots />
+            <span>{post?.numComments}</span>
+          </div>
         </div>
         <div
+          className={style.post_icon}
           onClick={() => {
             setOpenShare(!openShare);
+            contenedor(post);
           }}
         >
           <IoArrowRedoOutline />
           <span>{post?.nShares}</span>
         </div>
+        <div className={style.post_icon}>
+          <BsTrophy />
+        </div>
+      </div>
+      <div
+        onClick={() => {
+          setOpenShare(!openShare);
+        }}
+      >
+        <IoArrowRedoOutline />
+        <span>{post?.nShares}</span>
       </div>
     </div>
   );
