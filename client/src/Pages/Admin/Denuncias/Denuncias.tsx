0fde4a2a3f1a8db2940012836408cf0f1517a614
pablo99nunez <pablo99nunez@gmail.com>
2022-03-09 19:36:42 -0300

    feat: Admin panel 100% functional

--- client/src/Pages/Admin/Denuncias/Denuncias.tsx
+++ client/src/Pages/Admin/Denuncias/Denuncias.tsx
@@ -1,5 +1,7 @@
+import axios from "axios";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { ErrorAlert, InfoAlert } from "../../../Components/Alert/Alert";
 import Button from "../../../Components/Button/Button";
 import Post from "../../../Components/Post/Post";
 import { getPosts } from "../../../redux/actions/actions";
@@ -9,6 +11,16 @@ import style from "./Denuncias.module.scss";
 export default function Denuncias() {
   const posts = useSelector((state: IState) => state.posts);
   const dispatch = useDispatch();
+  const handleDelete = async (_id: string) => {
+    try {
+      await axios.delete("/post", { data: { _id } });
+      InfoAlert.fire("Exito");
+      dispatch(getPosts());
+    } catch (error) {
+      ErrorAlert.fire("Hubo un error" + error);
+    }
+  };
+
   useEffect(() => {
     dispatch(getPosts());
     console.log(
@@ -29,7 +41,13 @@ export default function Denuncias() {
               <Post post={e}></Post>
               <div className={style.options}>
                 <h3>Numero de reportes: {e.reportedTimes}</h3>
-                <Button>Eliminar publicacion</Button>
+                <Button
+                  onClick={() => {
+                    handleDelete(e._id);
+                  }}
+                >
+                  Eliminar publicacion
+                </Button>
               </div>
             </div>
           );
--- client/src/Pages/Admin/ListUsers/ListUsers.tsx
+++ client/src/Pages/Admin/ListUsers/ListUsers.tsx
@@ -6,7 +6,7 @@ import { ErrorAlert, InfoAlert } from "../../../Components/Alert/Alert";
 import Button from "../../../Components/Button/Button";
 import Modal from "../../../Components/Modal/Modal";
 import useUser from "../../../Hooks/useUser";
-import { searchUsers } from "../../../redux/actions/actions";
+import { editUser, searchUsers } from "../../../redux/actions/actions";
 import { handleDeleteUser } from "../../User/User";
 type Props = {
   users: IUser[];
@@ -15,8 +15,9 @@ import style from "./ListUsers.module.scss";
 
 export default function ListUsers({ users }: Props) {
   const userLog = useUser();
-  const [editRole, setEditRole] = useState(false);
+  const [user, setUser] = useState<IUser>();
   const [role, setRole] = useState("");
+  const [editRole, setEditRole] = useState(false);
   const dispatch = useDispatch();
 
   const deleteUser = (id: string | undefined, uid: string) => {
@@ -28,6 +29,20 @@ export default function ListUsers({ users }: Props) {
   const makeAdmin = async (username: string) => {
     try {
       await axios.post("/admin", { username });
+      InfoAlert.fire("Exito");
+      dispatch(searchUsers(""));
+    } catch (error) {
+      ErrorAlert.fire("Hubo un error" + error);
+    }
+  };
+  const changeRole = (e: any) => {
+    e.preventDefault();
+    setRole(e.target.value);
+  };
+  const submitRole = async (id: string, changes: any) => {
+    try {
+      await axios.put("/user", { _id: id, changes });
+      setEditRole(false);
       InfoAlert.fire("Exito");
       dispatch(searchUsers(""));
     } catch (error) {
@@ -68,7 +83,10 @@ export default function ListUsers({ users }: Props) {
               <Button
                 backgroundColor="#0c0c0c"
                 disabled={(user.admin || user.master) && !userLog?.master}
-                onClick={() => setEditRole(true)}
+                onClick={() => {
+                  setUser(user);
+                  setEditRole(true);
+                }}
               >
                 Cambiar rol
               </Button>
@@ -84,33 +102,41 @@ export default function ListUsers({ users }: Props) {
                 </Button>
               )}
             </div>
-            <Modal isOpen={editRole} setIsOpen={setEditRole} title="Editar Rol">
-              <div className={style.buttons}>
-                <Button
-                  active={(role || user.role) == "TA"}
-                  onClick={() => setRole("TA")}
-                >
-                  TA
-                </Button>
-                <Button
-                  active={(role || user.role) == "Estudiante"}
-                  onClick={() => setRole("Estudiante")}
-                >
-                  Estudiante
-                </Button>
-                <Button
-                  active={(role || user.role) == "Instructor"}
-                  onClick={() => setRole("Instructor")}
-                >
-                  Instructor
-                </Button>
-              </div>
-            </Modal>
           </li>
         ))
       ) : (
         <h3>No hay usuarios</h3>
       )}
+      <Modal isOpen={editRole} setIsOpen={setEditRole} title="Editar Rol">
+        <div className={style.wrapEditRole}>
+          <h2>Selecciona el rol:</h2>
+          <select className={style.editRole} onChange={changeRole}>
+            <option
+              value="Estudiante"
+              selected={user?.role === "Estudiante" ? true : false}
+            >
+              Estudiante
+            </option>
+            <option value="TA" selected={user?.role === "TA" ? true : false}>
+              TA
+            </option>
+            <option
+              value="Instructor"
+              selected={user?.role === "Instructor" ? true : false}
+            >
+              Instructor
+            </option>
+          </select>
+        </div>
+        <Button
+          onClick={() => {
+            user?._id && submitRole(user._id, { role });
+            setUser(undefined);
+          }}
+        >
+          Aceptar
+        </Button>
+      </Modal>
     </ul>
   );
 }
